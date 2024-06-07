79f88db3ad261940c63afbf0d06404a5616d5f9f
8igmac <8igmac@pm.me>
2024-06-07 18:51:18 +0800

    ch1-ch5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+// Use this file to keep the fonts used through out the app.
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({ 
+  subsets: ["latin"],
+  weight: "700"
+});
